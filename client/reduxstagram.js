06e6c4ff94d7f9f046ec4e5163d402dd0c754a32
Austin Sims <adsims2001@gmail.com>
2017-04-01 12:33:11 -0400

    added jsx

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,3 +2,5 @@ import React from 'react';
 import {render} from 'react-dom';
 import css from './styles.css';
 
+const rootEl = document.getElementById('root');
+render(<h1>Hello, JSX</h1>, rootEl);
