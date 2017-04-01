9de980f9dac0498e13dc1ec456c48c4358cfe3c5
Austin Sims <adsims2001@gmail.com>
2017-04-01 13:45:54 -0400

    we don't need Main

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,5 @@
 import {render} from 'react-dom';
 import css from './styles.css';
-import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 import React from 'react';
