8a47da9d8efae4703d3aba5d580648c6ffbb9241
Austin Sims <adsims2001@gmail.com>
2017-04-01 19:13:00 -0400

    Link to Single page from each Photo

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Photo({post}) {
-  return (<figure className = "grid-figure">
-    Post ID: {post.postId}
+  return (<figure className="grid-figure">
+    <div className="grid-photo-wrap">
+      <Link to={'/view/' + post.postId}>
+        {post.caption}
+      </Link>
+    </div>
   </figure>);
 }
 
