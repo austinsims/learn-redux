032af7850d46528249dfbe9ce8d762a408f922a2
Austin Sims <adsims2001@gmail.com>
2017-04-01 18:21:10 -0400

    fix routing.  Routes must be direct children of Router

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,16 +1,19 @@
-import {Link, Route} from 'react-router-dom';
+import {Link, BrowserRouter, Route} from 'react-router-dom';
 import PhotoGrid from './PhotoGrid';
 import React from 'react';
 import Single from './Single';
+import {history} from '../store';
 
 class Main extends React.Component {
   render() {
     return (
-      <div>
-        <h1><Link to="/">Reduxtagram</Link></h1>
-        <Route exact path="/" component={PhotoGrid} />
-        <Route path="/view/:photoId" component={Single} />
-      </div>
+      <BrowserRouter history={history}>
+        <div>
+          <h1><Link to="/">Reduxtagram</Link></h1>
+          <Route exact path="/" component={PhotoGrid} />
+          <Route path="/view/:photoId" component={Single} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,18 +1,15 @@
-import {Link, BrowserRouter, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
 import css from './styles.css';
 import PhotoGrid from './components/PhotoGrid';
 import React from 'react';
 import Single from './components/Single';
-import store, {history} from './store';
+import store from './store';
 import App from './components/App';
 
 const router = (
   <Provider store={store}>
-    <BrowserRouter history={history}>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
 
