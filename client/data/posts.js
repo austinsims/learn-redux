ca361bcb6abdc55047751577f17c097ba474d3bc
Austin Sims <adsims2001@gmail.com>
2017-04-01 19:20:03 -0400

    delete the pics with babies in them

--- client/data/posts.js
+++ client/data/posts.js
@@ -35,13 +35,6 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg"
    },
    {
-      "postId":"-B3eiIwcYV",
-      "caption":"Tacos for breakfast. I love you Austin. 🇺🇸",
-      "likes":33,
-      "id":"1117418173361145365",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/11917950_927755223968499_1198055371_n.jpg"
-   },
-   {
       "postId":"BAhvZrRwcfu",
       "caption":"Tried poke for the first time at @pokehbar. Delicious! It's like a bowl of sushi",
       "likes":30,
@@ -56,20 +49,6 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/1516572_445736812276082_2116173059_n.jpg"
    },
    {
-      "postId":"_4jHytwcUA",
-      "caption":"2015 can be summed up with one baby and a many lines of code. And sometimes a coding baby. 👶🏼⌨",
-      "likes":62,
-      "id":"1150824171912152320",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/10723795_1149927178351091_1859033096_n.jpg"
-   },
-   {
-      "postId":"_zbaOlQcbn",
-      "caption":"Lekker Chocoladeletter",
-      "likes":52,
-      "id":"1149382879529256679",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12346073_1035047523184672_768982339_n.jpg"
-   },
-   {
       "postId":"_rmvQfQce8",
       "caption":"Just discovered the #hamont farmers market has a new ramen place! 🍜",
       "likes":35,
@@ -77,25 +56,11 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12331739_1671776806423597_995664526_n.jpg"
    },
    {
-      "postId":"_ep9kiQcVy",
-      "caption":"⛄️",
-      "likes":64,
-      "id":"1143535906423162226",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12354078_447337935474115_1484398925_n.jpg"
-   },
-   {
       "postId":"_XpJcrwcSn",
       "caption":"6 page spread on flexbox in this months netmag!",
       "likes":74,
       "id":"1141561999742846119",
       "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12362588_1688046211438811_1395882545_n.jpg"
-   },
-   {
-      "postId":"_KnU7MwceA",
-      "caption":"Hanging out in my office waiting for 5:00 beers to come around.",
-      "likes":54,
-      "id":"1137894817632733056",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12301208_1533749386944985_1334730917_n.jpg"
    },
    {
       "postId":"_HMejJQcY5",
@@ -126,25 +91,11 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12331333_1650987978502155_1162510634_n.jpg"
    },
    {
-      "postId":"-pjx-gQcVi",
-      "caption":"My baby and me. Thanks to @bearandsparrow for this one.",
-      "likes":81,
-      "id":"1128590547628442978",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12298962_863814057068027_460827278_n.jpg"
-   },
-   {
       "postId":"-oTZ0zQcWt",
       "caption":"It's too early. Send coffee.",
       "likes":81,
       "id":"1128237044221461933",
       "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12328347_990748230999662_1512917342_n.jpg"
-   },
-   {
-      "postId":"-mxKQoQcQh",
-      "caption":"They both have figured it out. #lifewithsnickers",
-      "likes":47,
-      "id":"1127804966031967265",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e35/12256736_1758525004381641_1136705181_n.jpg"
    },
    {
       "postId":"-fasqlQceO",
@@ -160,13 +111,6 @@ const posts = [
       "id":"1122810327591928991",
       "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12224456_175248682823294_1558707223_n.jpg"
    },
-   {
-      "postId":"-FpTyHQcau",
-      "caption":"I'm in Austin for a conference and doing some training. Enjoying some local brew with my baby.",
-      "likes":82,
-      "id":"1118481761857291950",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/11326072_550275398458202_1726754023_n.jpg"
-   }
 ];
 
 
