e55b08438828cb42dbf3d5371c253e616c59f0a5
Austin Sims <adsims2001@gmail.com>
2017-04-01 19:30:30 -0400

    show comment count, like button, etc

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-function Photo({post}) {
+function Photo({post, commentCount}) {
   return (<figure className="grid-figure">
     <div className="grid-photo-wrap">
       <Link to={'/view/' + post.postId}>
@@ -10,6 +10,15 @@ function Photo({post}) {
     </div>
     <figcaption>
       <p>{post.caption}</p>
+      <div className="control-buttons">
+        <button className="likes">&hearts; {post.likes}</button>
+        <Link className="button" to={'/view/' + post.postId}>
+          <span className="comment-count">
+            <span className="speech-bubble"></span>
+            {commentCount}
+          </span>
+        </Link>
+      </div>
     </figcaption>
   </figure>);
 }
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -3,10 +3,19 @@ import {Link} from 'react-router-dom';
 import React from 'react';
 import Photo from './Photo';
 
-function PhotoGrid({posts}) {
+function PhotoGrid({posts, comments}) {
   return (<div className="photo-grid">
-    {posts.map((post, i) => <Photo post={post} key={i} />)}
+    {
+      posts.map((post, i) => {
+        const commentsOnPost = comments[post.postId] || [];
+        const commentCount = commentsOnPost.length;
+        return <Photo post={post} commentCount={commentCount} key={i} />
+      })
+    }
   </div>);
 }
 
-export default connect(state => ({posts: state.posts}))(PhotoGrid);
+export default connect(state => ({
+  posts: state.posts,
+  comments: state.comments,
+}))(PhotoGrid);
