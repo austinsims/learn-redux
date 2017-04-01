204ebb588882f2f070e914040004906998d8f478
Austin Sims <adsims2001@gmail.com>
2017-04-01 18:08:08 -0400

    fix some bad names

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -7,12 +7,12 @@ export function increment(index) {
 }
 
 // add comment
-export function addComment(postId, author, comment) {
+export function addComment(postId, user, text) {
   return {
     type: 'ADD_COMMENT',
     postId,
-    author,
-    comment,
+    user,
+    text,
   };
 }
 
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,15 @@
-export default function comments(state = [], action) {
-  console.log('comments reducer');
-  return state;
+/**
+ * @param {!Object<!Array<Object>>} state Object where keys are postId,
+ *     and values are an array of comment objects (strings user and comment)
+ * @param {*} action 
+ */
+export default function comments(state = {}, action) {
+  if (action.type !== 'ADD_COMMENT') {
+    return state;
+  }
+  const {postId, user, text} = action;
+  let commentsOnPost = state[postId] || [];
+  commentsOnPost = [...commentsOnPost, {user, text}];
+  console.log('new state: ', {...state, [postId]: commentsOnPost});
+  return {...state, postId: commentsOnPost};
 }
