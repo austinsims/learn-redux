62209d04d46f8c207e0e75fa4f11c8bd42f67c9a
Austin Sims <adsims2001@gmail.com>
2017-04-01 15:38:13 -0400

    hook up store to router with Provider

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,7 +1,6 @@
 import {Link, BrowserRouter, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 import css from './styles.css';
 import PhotoGrid from './components/PhotoGrid';
 import React from 'react';
@@ -9,13 +8,15 @@ import Single from './components/Single';
 import store, {history} from './store';
 
 const router = (
-  <BrowserRouter history={createBrowserHistory()}>
-    <div>
-      <h1><Link to="/">Reduxtagram</Link></h1>
-      <Route exact path="/" component={PhotoGrid} />
-      <Route path="/view/:photoId" component={Single} />
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter history={history}>
+      <div>
+        <h1><Link to="/">Reduxtagram</Link></h1>
+        <Route exact path="/" component={PhotoGrid} />
+        <Route path="/view/:photoId" component={Single} />
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 const rootEl = document.getElementById('root');
